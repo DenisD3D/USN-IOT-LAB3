d3451c490d7941b38bc14dfc68c4b22f388e3226
DenisD3D <dd.denis2544@gmail.com>
2023-11-14 11:46:11 +0100

    Fix usb6008.py to use two separate tasks

--- utils/usb6008.py
+++ utils/usb6008.py
@@ -4,19 +4,32 @@ from nidaqmx.constants import TerminalConfiguration
 
 class USB6008:
     def __init__(self):
-        self.task = nidaqmx.Task()
-        self.task.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config=TerminalConfiguration.DIFF)
-        self.task.ao_channels.add_ao_voltage_chan("Dev1/ao0", min_val=0, max_val=5)
-        self.task.timing.cfg_samp_clk_timing(1000, sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
-        self.task.start()
+        self.read_task = nidaqmx.Task()
+        self.write_task = nidaqmx.Task()
+        self.read_task.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config=TerminalConfiguration.DIFF)
+        self.write_task.ao_channels.add_ao_voltage_chan("Dev1/ao0", min_val=0, max_val=5)
+        self.read_task.start()
+        self.write_task.start()
 
     def read(self):
-        return self.task.read()
+        return self.read_task.read()
 
     def write(self, value):
-        self.task.write(value)
+        self.write_task.write(value)
 
     def close(self):
         self.write(0)
-        self.task.stop()
-        self.task.close()
+        self.read_task.close()
+        self.write_task.close()
+
+
+if __name__ == "__main__":
+    import time
+
+    usb6008 = USB6008()
+
+    usb6008.write(2.5)
+    time.sleep(1)
+    print(usb6008.read())
+
+    usb6008.close()
